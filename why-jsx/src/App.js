725c785b942ece7429e4b6a8254e2e267bae6cb1
hyuraku <yu@hill-wood.com>
2018-07-17 19:50:58 +0900

    add explanation for jsx

--- why-jsx/src/App.js
+++ why-jsx/src/App.js
@@ -1,9 +1,20 @@
+// jsxの使用にはReactが必要
 import React, { Component } from 'react';
 
 // <div>~</div>の箇所がjsx(javascript xml)
+// classを書くときは"className"と書く
 class App extends Component {
   render() {
-    return <div><h1>Hello World!!</h1></div>;
+    // const greeting = "Hi, Tom!!"
+    // const dom = <h1 className="foo">{greeting}</h1>
+    // return dom;
+    //
+    return (
+      <div>
+      <label htmlFor="bar">Bar</label>
+      <input type="button" onClick={() => console.log("Clicked")}/>
+      </div>
+    )
   }
 }
 
