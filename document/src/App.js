2c4e84ccdf33ea63fc35db5adb3a60250c8ddfbb
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-07-13 01:30:17 +0900

    greed

--- document/src/App.js
+++ document/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
 
+const name = "David"
+const greeding = <h1>Hello {name}</h1>;
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    greeding
   );
 }
 
