853a69c5f145a678c8eb22c35bdea259e0ae466b
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-07-14 22:33:52 +0900

    split 2

--- document/src/App.js
+++ document/src/App.js
@@ -9,12 +9,7 @@ function formatDate(date) {
 function Comment(props) {
   return (
     <div className="Comment">
-      <div className="UserInfo">
-        <Avatar user={props.author}/>
-        <div className="UserInfo-name">
-          {props.author.name}
-        </div>
-      </div>
+      <UserInfo user={props.author}/>
       <div className="Comment-text">{props.text}</div>
       <div className="Comment-date">
         {formatDate(props.date)}
@@ -31,6 +26,17 @@ function Avatar(props){
       alt={props.user.name}
     />
   );
+}
+
+function UserInfo(props){
+  return(
+    <div className="UserInfo">
+    <Avatar user={props.user}/>
+    <div className="UserInfo-name">
+      {props.user.name}
+    </div>
+  </div>
+  )
 }
 
 const comment = {
