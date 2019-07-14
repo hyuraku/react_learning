4e4b3152710b441bdeaa2bc934189b4d900d949c
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-07-14 22:24:14 +0900

    start to split component

--- document/src/App.js
+++ document/src/App.js
@@ -1,21 +1,48 @@
 import React from 'react';
 import './App.css';
 
-function Welcome(props){
-  return <h1>Hello, {props.name}</h1>
+function formatDate(date) {
+  return date.toLocaleDateString();
 }
 
-const element = (
-  <div>
-    <Welcome name="Tom"/>
-    <Welcome name="David"/>
-    <Welcome name="John"/>
-  </div>
-);
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <div className="UserInfo">
+        <img
+          className="Avatar"
+          src={props.author.avatarUrl}
+          alt={props.author.name}
+        />
+        <div className="UserInfo-name">
+          {props.author.name}
+        </div>
+      </div>
+      <div className="Comment-text">{props.text}</div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+    </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'https://placekitten.com/g/64/64',
+  },
+};
 
 function App() {
   return (
-    element
+    <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author}
+  />
   );
 }
 
