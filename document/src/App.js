04e8c1d773e8e69d94ab1247bc03e6d73cc73992
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-07-15 16:56:44 +0900

    to show clock

--- document/src/App.js
+++ document/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import './App.css';
 
-function Clock(props) {
-  return <h1>Clock</h1>;
+class Clock extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {date: new Date()}
+  }
+  render(){
+    return(
+      <div>
+        <h1>Hello World</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
-    <Clock/>
+    <Clock />
   );
 }
 
