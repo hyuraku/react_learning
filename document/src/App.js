d6ba4e48f75aadcd3db9d81f231214ff740c6e6d
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-07-15 17:05:26 +0900

    props to state

--- document/src/App.js
+++ document/src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import './App.css';
 
+function FormattedDate(props){
+  return <h2>It is {props.date.toLocaleTimeString()}</h2>
+}
+
 class Clock extends React.Component{
   constructor(props){
     super(props);
@@ -20,12 +24,15 @@ class Clock extends React.Component{
       date: new Date()
     })
   }
+  // never write below
+  // this.state.date = new Date()
 
   render(){
     return(
       <div>
         <h1>Hello World</h1>
         <h2>It is {this.state.date.toLocaleTimeString()}</h2>
+        <FormattedDate date={this.state.date}/>
       </div>
     )
   }
