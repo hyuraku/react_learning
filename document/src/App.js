3bbae9e6f909375ff1ca1fb49c69b154b45336ae
hyuraku <32809703+hyuraku@users.noreply.github.com>
2019-07-15 16:49:20 +0900

    start

--- document/src/App.js
+++ document/src/App.js
@@ -1,60 +1,13 @@
 import React from 'react';
 import './App.css';
 
-function formatDate(date) {
-  return date.toLocaleDateString();
+function Clock(props) {
+  return <h1>Clock</h1>;
 }
-
-
-function Comment(props) {
-  return (
-    <div className="Comment">
-      <UserInfo user={props.author}/>
-      <div className="Comment-text">{props.text}</div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-    </div>
-  );
-}
-
-function Avatar(props){
-  return(
-    <img
-      className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
-  );
-}
-
-function UserInfo(props){
-  return(
-    <div className="UserInfo">
-    <Avatar user={props.user}/>
-    <div className="UserInfo-name">
-      {props.user.name}
-    </div>
-  </div>
-  )
-}
-
-const comment = {
-  date: new Date(),
-  text: 'I hope you enjoy learning React!',
-  author: {
-    name: 'Hello Kitty',
-    avatarUrl: 'https://placekitten.com/g/64/64',
-  },
-};
 
 function App() {
   return (
-    <Comment
-    date={comment.date}
-    text={comment.text}
-    author={comment.author}
-  />
+    <Clock/>
   );
 }
 
