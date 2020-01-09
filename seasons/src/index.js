6934336837ad25421ee7a9948e47c8a467c451e9
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-01-09 23:22:42 +0900

    initialize state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 import SeasonDisplay from './SeasonDisplay'
 
-const App = () =>{
-  return <div><SeasonDisplay/></div>
+class App extends React.Component{
+  constructor(props){
+    super(props);
+
+    this.state = {lat: null}
+  }
+
+  render(){
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      err => console.log(err),
+    );
+
+    return <div>Latitude: </div>
+  }
 }
 
 ReactDOM.render(
