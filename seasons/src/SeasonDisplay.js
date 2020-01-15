9e876f3adead31c02be2ff3a1ef5fd17cb32b5a6
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-01-15 23:06:03 +0900

    add css

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,3 +1,4 @@
+import './SeasonDisplay.css'
 import React from 'react';
 
 const seasonConfig = {
@@ -21,14 +22,15 @@ const getSeason = (lat, month) =>{
 }
 
 const SeasonDisplay = (props) => {
-  const season = getSeason(props.lat, new Date().getMonth());
+  // const season = getSeason(props.lat, new Date().getMonth());
+  var season = "summer"
   const { text, iconName } = seasonConfig[season]
 
   return (
-    <div>
-      <i className={`${iconName} icon`}/>
-      <h1>{ text }</h1>
-      <i className={`${iconName} icon`}/>
+    <div className={`season-display ${season}`}>
+      < i className = {`icon-left massive ${iconName} icon`}      />
+        <h1>{ text }</h1>
+      <i className={`icon-right massive ${iconName} icon`}/>
     </div>
   )
 }
