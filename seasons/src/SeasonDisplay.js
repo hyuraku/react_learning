fcad81c310a58c8105703887e6a6533e888415c1
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-01-15 22:54:49 +0900

    setup seasonConfig

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,5 +1,16 @@
 import React from 'react';
 
+const seasonConfig = {
+  winter: {
+    text: 'Burr, it is chilly',
+    iconName: 'snowflake'
+  },
+  summer: {
+    text: 'Let hit the beach',
+    iconName: 'sun'
+  }
+}
+
 const getSeason = (lat, month) =>{
   const target_months = [3,4,5,6,7,8]
   if(target_months.includes(month)){
@@ -11,8 +22,15 @@ const getSeason = (lat, month) =>{
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth());
-  const text = season === 'winter' ? 'Burr, it is chilly' : 'Let hit the beach'
-  return <div>{ text }</div>
+  const { text, iconName } = seasonConfig[season]
+
+  return (
+    <div>
+      <i className={`${iconName} icon`}/>
+      <h1>{ text }</h1>
+      <i className={`${iconName} icon`}/>
+    </div>
+  )
 }
 
 export default SeasonDisplay
