d7205f1b3585a0bfbf4de27a5d9acbedac664d2f
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-01-27 23:50:20 +0900

    invoke callback

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import SearchBar from './SearchBar'
+import { render } from '@testing-library/react';
 
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: '20px'}}>
-      < SearchBar />
-    </div>
+class  App extends React.Component{
+  onSearchSubmit(term){
+    console.log(term)
+  }
+  render(){
+      return ( 
+      <div className = "ui container"  style = {{marginTop: '20px'} } >
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
     )
+  }
 };
 
 
--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,18 +1,26 @@
 import React from 'react'
 
 class SearchBar extends React.Component{
-  onInputChange(event){
-    console.log(event.target.value);
-  }
+  state = { term: '' }
 
+  onFormSubmit=(event)=>{
+    event.preventDefault();
+
+    // console.log(this.state.term)
+    this.props.onSubmit(this.state.term)
+  }
 
   render(){
     return(
       <div className="ui segemnt">
-        <form className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type = "text" onChange={this.onInputChange}/>
+            <input 
+              type = "text"
+              value={this.state.term}
+              onChange={(e)=> this.setState({ term: e.target.value.toUpperCase() })}
+            />
           </div>
         </form>
       </div>
