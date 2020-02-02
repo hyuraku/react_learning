3907fe472c8b41a0c3f05332ff9c23d0fe519df5
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-02-02 22:48:37 +0900

    show image list

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,7 +1,22 @@
+import './ImageList.css'
 import React from 'react'
 
-const ImageList = () => {
-  return <div>Image List</div>
+const ImageList = (props) => {
+  console.log(props.images)
+
+  // return <div>Image List</div>
+  const images = props.images.map((image)=>{
+    return (
+      <div key={image.id} className="oneImage">
+        <img
+          key={image.id}
+          alt={image.description}
+          src={image.urls.regular}/>
+      </div>
+    )
+  })
+
+  return <div className="image-list">{images}</div>
 }
 
 export default ImageList
