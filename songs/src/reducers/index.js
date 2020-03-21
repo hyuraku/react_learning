53c904abf78e4014a3698feab5d8ae0d508bd8f9
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-03-21 23:22:47 +0900

    create action and reducer

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,17 @@
+const songsReducer = ()=>{
+  return [
+    {title: "a", duration: '4;05'},
+    {
+      title: "s",
+      duration: '4;05'
+    },
+    {
+      title: "d",
+      duration: '4;05'
+    },
+    {
+      title: "f",
+      duration: '4;05'
+    },
+  ]
+}
