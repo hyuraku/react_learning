ad120121e0c6cc322c0668284517729dd88156b3
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-03-26 09:18:02 +0900

    add language selector

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,7 +1,22 @@
 import React from "react"
 
-const App = () => {
-  return <div>App</div>
+class App extends React.Component{
+  state = {language: "English"}
+  onLanguageChanged = (language) =>{
+    this.setState({language: language})
+  }
+  render(){
+    return (
+      <div className="ui container">
+        <div>
+            Select Language
+            <i className="flag us" onClick={()=> this.onLanguageChanged('English')}></i>
+            <i className="flag nl" onClick={()=> this.onLanguageChanged('Dutch')}></i>
+        </div>
+        {this.state.language}
+      </div>
+    )
+  }  
 }
 
 export default App
