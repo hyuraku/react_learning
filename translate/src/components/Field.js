ed179e96b242280ac1d839505c620348987d421d
hyuraku <32809703+hyuraku@users.noreply.github.com>
2020-03-28 18:46:18 +0900

    create field

---
+++ translate/src/components/Field.js
@@ -0,0 +1,12 @@
+import React, { useDebugValue } from 'react'
+
+class Field extends React.Component{
+  render(){
+    return(
+      <div className='ui filed'>
+        <label>Name</label>
+        <input></input>
+      </div>
+    )
+  }
+}
